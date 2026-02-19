082215cc24b877b7b4a6f9aa7a21819a6ada8104
4nandaw <ananda.vilar.vidal@ccc.ufcg.edu.br>
2026-02-19 19:40:29 -0300

    feat: create MainLayout component to structure the application with Header, Footer, and routing support

---
+++ frontend/src/app/components/layout/MainLayout.jsx
@@ -0,0 +1,20 @@
+import { Outlet } from "react-router";
+import { Header } from "./Header";
+import { Footer } from "./Footer";
+import { CartProvider } from "../../context/CartContext";
+import { Toaster } from "sonner";
+
+export function MainLayout() {
+  return (
+    <CartProvider>
+      <div className="flex min-h-screen flex-col bg-slate-50 font-sans">
+        <Header />
+        <main className="flex-1 w-full">
+          <Outlet />
+        </main>
+        <Footer />
+        <Toaster position="top-right" />
+      </div>
+    </CartProvider>
+  );
+}
